5a67a51338a3f2fb41a8225ca2ed49c66acf6a5d
ambebe <andreykaraniklev@gmail.com>
2026-04-19 18:01:22 +0300

    project created

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,7 +4,7 @@ from mainpage import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='base'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/', edit_profile, name='profile')
 ]
